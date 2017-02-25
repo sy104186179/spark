10f1f7452b5c9600784fd4559ee2561205e4cb9c
Yuming Wang <wgyumg@gmail.com>
2017-02-25 12:45:23 +0800

    queue name

--- resource-managers/yarn/src/test/scala/org/apache/spark/deploy/yarn/DynamicSetMaxExecutorsSuite.scala
+++ resource-managers/yarn/src/test/scala/org/apache/spark/deploy/yarn/DynamicSetMaxExecutorsSuite.scala
@@ -52,72 +52,46 @@ import org.apache.spark.util.Utils
 @ExtendedYarnTest
 class DynamicSetMaxExecutorsSuite extends BaseYarnClusterSuite {
 
-  val ra = CapacitySchedulerConfiguration.ROOT + ".ra"
-  val rb = CapacitySchedulerConfiguration.ROOT + ".rb"
-  val a1 = ra + ".a1"
-  val a2 = ra + ".a2"
-
-  val aCapacity = 40F
-  val aMaximumCapacity = 60F
-  val bCapacity = 60F
-  val bMaximumCapacity = 100F
-  val a1Capacity = 30F
-  val a1MaximumCapacity = 70F
-  val a2Capacity = 70F
-
   val cpuCores = 8
   val numNodeManagers = 10
   val coresTotal = cpuCores * numNodeManagers
 
-  // Disable the disk utilization check to avoid the test hanging when people's disks are
-  // getting full.
-  val yarnConf = new CapacitySchedulerConfiguration()
+  override def newYarnConfig(): CapacitySchedulerConfiguration = {
+    val ra = CapacitySchedulerConfiguration.ROOT + ".ra"
+    val rb = CapacitySchedulerConfiguration.ROOT + ".rb"
+    val a1 = ra + ".a1"
+    val a2 = ra + ".a2"
 
-  // Define top-level queues
-  yarnConf.setQueues(CapacitySchedulerConfiguration.ROOT, Array("ra", "rb"))
-  yarnConf.setMaximumCapacity(CapacitySchedulerConfiguration.ROOT, 100)
-  yarnConf.setCapacity(ra, aCapacity)
-  yarnConf.setMaximumCapacity(ra, aMaximumCapacity)
-  yarnConf.setCapacity(rb, bCapacity)
-  yarnConf.setMaximumCapacity(rb, bMaximumCapacity)
+    val aCapacity = 40F
+    val aMaximumCapacity = 60F
+    val bCapacity = 60F
+    val bMaximumCapacity = 100F
+    val a1Capacity = 30F
+    val a1MaximumCapacity = 70F
+    val a2Capacity = 70F
 
-  // Define 2nd-level queues
-  yarnConf.setQueues(ra, Array("a1", "a2"))
-  yarnConf.setCapacity(a1, a1Capacity)
-  yarnConf.setMaximumCapacity(a1, a1MaximumCapacity)
-  yarnConf.setCapacity(a2, a2Capacity)
-  yarnConf.set("yarn.nodemanager.resource.cpu-vcores", cpuCores.toString)
-  yarnConf.set("yarn.nodemanager.disk-health-checker.max-disk-utilization-per-disk-percentage",
-    "100.0")
+    // Disable the disk utilization check to avoid the test hanging when people's disks are
+    // getting full.
+    val yarnConf = new CapacitySchedulerConfiguration()
 
-  override def newYarnConfig(): Configuration = yarnConf
+    // Define top-level queues
+    yarnConf.setQueues(CapacitySchedulerConfiguration.ROOT, Array("ra", "rb"))
+    yarnConf.setMaximumCapacity(CapacitySchedulerConfiguration.ROOT, 100)
+    yarnConf.setCapacity(ra, aCapacity)
+    yarnConf.setMaximumCapacity(ra, aMaximumCapacity)
+    yarnConf.setCapacity(rb, bCapacity)
+    yarnConf.setMaximumCapacity(rb, bMaximumCapacity)
 
-  private val TEST_PYFILE = """
-    |import mod1, mod2
-    |import sys
-    |from operator import add
-    |
-    |from pyspark import SparkConf , SparkContext
-    |if __name__ == "__main__":
-    |    if len(sys.argv) != 2:
-    |        print >> sys.stderr, "Usage: test.py [result file]"
-    |        exit(-1)
-    |    sc = SparkContext(conf=SparkConf())
-    |    status = open(sys.argv[1],'w')
-    |    result = "failure"
-    |    rdd = sc.parallelize(range(10)).map(lambda x: x * mod1.func() * mod2.func())
-    |    cnt = rdd.count()
-    |    if cnt == 10:
-    |        result = "success"
-    |    status.write(result)
-    |    status.close()
-    |    sc.stop()
-    """.stripMargin
-
-  private val TEST_PYMODULE = """
-    |def func():
-    |    return 42
-    """.stripMargin
+    // Define 2nd-level queues
+    yarnConf.setQueues(ra, Array("a1", "a2"))
+    yarnConf.setCapacity(a1, a1Capacity)
+    yarnConf.setMaximumCapacity(a1, a1MaximumCapacity)
+    yarnConf.setCapacity(a2, a2Capacity)
+    yarnConf.set("yarn.nodemanager.resource.cpu-vcores", cpuCores.toString)
+    yarnConf.set("yarn.nodemanager.disk-health-checker.max-disk-utilization-per-disk-percentage",
+      "100.0")
+    yarnConf
+  }
 
   test("run Spark in yarn-client mode") {
     testBasicYarnApp(true)
@@ -169,28 +143,6 @@ class DynamicSetMaxExecutorsSuite extends BaseYarnClusterSuite {
   test("run Spark in yarn-cluster mode failure after sc initialized") {
     val finalState = runSpark(false, mainClassName(YarnClusterDriverWithFailure.getClass))
     finalState should be (SparkAppHandle.State.FAILED)
-  }
-
-  test("run Python application in yarn-client mode") {
-    testPySpark(true)
-  }
-
-  test("run Python application in yarn-cluster mode") {
-    testPySpark(false)
-  }
-
-  test("run Python application in yarn-cluster mode using " +
-    " spark.yarn.appMasterEnv to override local envvar") {
-    testPySpark(
-      clientMode = false,
-      extraConf = Map(
-        "spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON"
-          -> sys.env.getOrElse("PYSPARK_DRIVER_PYTHON", "python"),
-        "spark.yarn.appMasterEnv.PYSPARK_PYTHON"
-          -> sys.env.getOrElse("PYSPARK_PYTHON", "python")),
-      extraEnv = Map(
-        "PYSPARK_DRIVER_PYTHON" -> "not python",
-        "PYSPARK_PYTHON" -> "not python"))
   }
 
   test("user class path first in client mode") {
@@ -268,49 +220,6 @@ class DynamicSetMaxExecutorsSuite extends BaseYarnClusterSuite {
       extraJars = Seq("local:" + originalJar.getPath()))
     checkResult(finalState, driverResult, "ORIGINAL")
     checkResult(finalState, executorResult, "ORIGINAL")
-  }
-
-  private def testPySpark(
-      clientMode: Boolean,
-      extraConf: Map[String, String] = Map(),
-      extraEnv: Map[String, String] = Map()): Unit = {
-    val primaryPyFile = new File(tempDir, "test.py")
-    Files.write(TEST_PYFILE, primaryPyFile, StandardCharsets.UTF_8)
-
-    // When running tests, let's not assume the user has built the assembly module, which also
-    // creates the pyspark archive. Instead, let's use PYSPARK_ARCHIVES_PATH to point at the
-    // needed locations.
-    val sparkHome = sys.props("spark.test.home")
-    val pythonPath = Seq(
-        s"$sparkHome/python/lib/py4j-0.10.4-src.zip",
-        s"$sparkHome/python")
-    val extraEnvVars = Map(
-      "PYSPARK_ARCHIVES_PATH" -> pythonPath.map("local:" + _).mkString(File.pathSeparator),
-      "PYTHONPATH" -> pythonPath.mkString(File.pathSeparator)) ++ extraEnv
-
-    val moduleDir =
-      if (clientMode) {
-        // In client-mode, .py files added with --py-files are not visible in the driver.
-        // This is something that the launcher library would have to handle.
-        tempDir
-      } else {
-        val subdir = new File(tempDir, "pyModules")
-        subdir.mkdir()
-        subdir
-      }
-    val pyModule = new File(moduleDir, "mod1.py")
-    Files.write(TEST_PYMODULE, pyModule, StandardCharsets.UTF_8)
-
-    val mod2Archive = TestUtils.createJarWithFiles(Map("mod2.py" -> TEST_PYMODULE), moduleDir)
-    val pyFiles = Seq(pyModule.getAbsolutePath(), mod2Archive.getPath()).mkString(",")
-    val result = File.createTempFile("result", null, tempDir)
-
-    val finalState = runSpark(clientMode, primaryPyFile.getAbsolutePath(),
-      sparkArgs = Seq("--py-files" -> pyFiles),
-      appArgs = Seq(result.getAbsolutePath()),
-      extraEnv = extraEnvVars,
-      extraConf = extraConf)
-    checkResult(finalState, result)
   }
 
   private def testUseClassPathFirst(clientMode: Boolean): Unit = {
