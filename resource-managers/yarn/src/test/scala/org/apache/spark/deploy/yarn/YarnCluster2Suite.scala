41d5f43e525f452ae4aaf78d04918690089e07a5
Yuming Wang <wgyumg@gmail.com>
2017-02-26 08:56:03 +0800

    set("spark.yarn.am.memory", "10M")

--- resource-managers/yarn/src/test/scala/org/apache/spark/deploy/yarn/YarnCluster2Suite.scala
+++ resource-managers/yarn/src/test/scala/org/apache/spark/deploy/yarn/YarnCluster2Suite.scala
@@ -97,7 +97,7 @@ private object YarnClusterDriverUseSparkHadoopUtilConf2 extends Logging with Mat
       sc = new SparkContext(new SparkConf().setSparkHome("/root/opensource/spark")
          .set("spark.dynamicAllocation.enabled", "true")
           .set("spark.shuffle.service.enabled", "true")
-         .set("spark.executor.memory", "10M")
+         .set("spark.executor.memory", "10M").set("spark.yarn.am.memory", "10M")
         .setAppName("yarn test using SparkHadoopUtil's conf"))
 
       Files.write(result, status, StandardCharsets.UTF_8)
