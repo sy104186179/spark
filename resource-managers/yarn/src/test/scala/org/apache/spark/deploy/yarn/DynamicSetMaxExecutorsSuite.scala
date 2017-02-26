f7bd1de7104ef0117fdb0c0814df3a0ea102246d
Yuming Wang <wgyumg@gmail.com>
2017-02-26 21:20:50 +0800

    executors

--- resource-managers/yarn/src/test/scala/org/apache/spark/deploy/yarn/DynamicSetMaxExecutorsSuite.scala
+++ resource-managers/yarn/src/test/scala/org/apache/spark/deploy/yarn/DynamicSetMaxExecutorsSuite.scala
@@ -83,54 +83,62 @@ class DynamicSetMaxExecutorsSuite extends BaseYarnClusterSuite {
   }
 
   test(s"run Spark on YARN with dynamicAllocation enabled and ${ queueNameA1 } queue") {
-    // a1's cores: 80 * 0.6 * 0.7 = 33
+    // a1's executors: 80 * 0.6 * 0.7 = 33
     setMaxExecutors(33, queueNameA1,
-      s"spark.dynamicAllocation.enabled=${isDynamicAllocation}," +
-        s"spark.shuffle.service.enabled=${isDynamicAllocation}")
+      s"spark.dynamicAllocation.enabled=${ isDynamicAllocation }," +
+        s"spark.shuffle.service.enabled=${ isDynamicAllocation }")
   }
 
   test(s"run Spark on YARN with dynamicAllocation enabled and ${ queueNameA2 } queue") {
-    // a2's cores: 80 * 0.6 * 1 = 48
+    // a2's executors: 80 * 0.6 * 1 = 48
     setMaxExecutors(48, queueNameA2,
-      s"spark.dynamicAllocation.enabled=${isDynamicAllocation}," +
-        s"spark.shuffle.service.enabled=${isDynamicAllocation}")
+      s"spark.dynamicAllocation.enabled=${ isDynamicAllocation }," +
+        s"spark.shuffle.service.enabled=${ isDynamicAllocation }")
   }
 
   test(s"run Spark on YARN with dynamicAllocation enabled and ${ queueNameRB } queue") {
-    // b's cores: 80 * 1 = 80
+    // b's executors: 80 * 1 = 80
     setMaxExecutors(80, queueNameRB,
-      s"spark.dynamicAllocation.enabled=${isDynamicAllocation}," +
-        s"spark.shuffle.service.enabled=${isDynamicAllocation}")
+      s"spark.dynamicAllocation.enabled=${ isDynamicAllocation }," +
+        s"spark.shuffle.service.enabled=${ isDynamicAllocation }")
   }
 
   test(s"run Spark on YARN with dynamicAllocation enabled and ${ queueNameA1 } queue and " +
     s"user set maxExecutors") {
     val executors = 12
     setMaxExecutors(executors, queueNameA1,
-      s"spark.dynamicAllocation.enabled=${isDynamicAllocation}," +
-        s"spark.shuffle.service.enabled=${isDynamicAllocation}" +
-        s",${DYN_ALLOCATION_MAX_EXECUTORS.key}=${executors}")
+      s"spark.dynamicAllocation.enabled=${ isDynamicAllocation }," +
+        s"spark.shuffle.service.enabled=${ isDynamicAllocation }" +
+        s",${DYN_ALLOCATION_MAX_EXECUTORS.key}=${ executors }")
   }
 
   test(s"run Spark on YARN with dynamicAllocation disabled and ${ queueNameA1 } queue") {
     setMaxExecutors(Int.MaxValue, queueNameA1,
-      s"spark.dynamicAllocation.enabled=${!isDynamicAllocation}," +
-        s"spark.shuffle.service.enabled=${!isDynamicAllocation}")
+      s"spark.dynamicAllocation.enabled=${ !isDynamicAllocation }," +
+        s"spark.shuffle.service.enabled=${ !isDynamicAllocation }")
   }
 
   test(s"run Spark on YARN with dynamicAllocation disabled and ${ queueNameA1 } queue and " +
     s"user set maxExecutors") {
     val executors = 12
     setMaxExecutors(executors, queueNameA1,
-      s"spark.dynamicAllocation.enabled=${isDynamicAllocation}," +
-        s"spark.shuffle.service.enabled=${isDynamicAllocation}" +
-        s",${DYN_ALLOCATION_MAX_EXECUTORS.key}=${executors}")
+      s"spark.dynamicAllocation.enabled=${ isDynamicAllocation }," +
+        s"spark.shuffle.service.enabled=${ isDynamicAllocation }" +
+        s",${DYN_ALLOCATION_MAX_EXECUTORS.key}=${ executors }")
+  }
+
+  test(s"run Spark on YARN with dynamicAllocation enabled and ${ queueNameRB } queue and " +
+    s"user set spark.executor.cores") {
+    // b's executors = 80 * 1 / 3 = 26
+    setMaxExecutors(26, queueNameA1,
+      s"spark.dynamicAllocation.enabled=${ isDynamicAllocation }," +
+        s"spark.shuffle.service.enabled=${ isDynamicAllocation }" +
+        s",${EXECUTOR_CORES.key}=3")
   }
 
   private def setMaxExecutors(expectedExecutors: Int,
                               queueName: String,
-                              extArgMaps: String
-                             ): Unit = {
+                              extArgMaps: String): Unit = {
     val result = File.createTempFile("result", null, tempDir)
     val finalState = runSpark(true,
       mainClassName(SetMaxExecutors.getClass),
