aed2a984e560dc2fa95362a8a4ec924357a35dd7
Yuming Wang <wgyumg@gmail.com>
2017-04-02 09:46:08 +0800

    revert

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -696,23 +696,23 @@ private[hive] class HiveClientImpl(
       isSrcLocal: Boolean): Unit = withHiveState {
     val tbl = client.getTable(tableName)
     val fs = tbl.getDataLocation.getFileSystem(conf)
-    if (replace) {
-      shim.moveFile(
-        client,
-        conf,
-        new Path(loadPath),
-        tbl.getPath,
-        fs,
-        replace,
-        isSrcLocal)
-    } else {
-      shim.loadTable(
-        client,
-        new Path(loadPath),
-        tableName,
-        replace,
-        isSrcLocal)
-    }
+//    if (replace) {
+//      shim.moveFile(
+//        client,
+//        conf,
+//        new Path(loadPath),
+//        tbl.getPath,
+//        fs,
+//        replace,
+//        isSrcLocal)
+//    } else {
+    shim.loadTable(
+      client,
+      new Path(loadPath),
+      tableName,
+      replace,
+      isSrcLocal)
+    // }
   }
 
   def loadDynamicPartitions(
