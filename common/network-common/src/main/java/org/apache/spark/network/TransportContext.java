41cfd75d29e34a61aedbb09b49bd550c7dfdc904
Yuming Wang <wgyumg@gmail.com>
2017-07-03 14:37:01 +0800

    Revert

--- common/network-common/src/main/java/org/apache/spark/network/TransportContext.java
+++ common/network-common/src/main/java/org/apache/spark/network/TransportContext.java
@@ -148,8 +148,6 @@ public class TransportContext {
       channel.pipeline()
         .addLast("encoder", ENCODER)
         .addLast(TransportFrameDecoder.HANDLER_NAME, NettyUtils.createFrameDecoder())
-        .addLast("codec", new HttpClientCodec(4096, 8192, 8192, false))
-        .addLast("clientCodec", new HttpClientCodec(4096, 8192, 8192, false))
         .addLast("decoder", DECODER)
         .addLast("idleStateHandler", new IdleStateHandler(0, 0, conf.connectionTimeoutMs() / 1000))
         // NOTE: Chunks are currently guaranteed to be returned in the order of request, but this
